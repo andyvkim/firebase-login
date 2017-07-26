32dcd1154b95fe695cac19be6a4c2048267279d7
andyvkim <andyvkim@gmail.com>
2017-07-26 13:21:11 -0400

    Add files via upload

--- src/Authen.js
+++ src/Authen.js
@@ -23,7 +23,12 @@ class Authen extends Component {
     const promise = auth.signInWithEmailAndPassword(email, password);
 
     promise
-    .then()
+    .then(user => {
+      var lout = document.getElementById("logout");
+      var err = "welcome back";
+      lout.classList.remove('hide');
+      this.setState({err:err});
+    })
 
     promise.catch(e => {
       var err = e.message;
@@ -55,6 +60,14 @@ class Authen extends Component {
       this.setState(({err:err}));
     });
   }
+  logout(){
+    firebase.auth().signOut;
+    var lout = document.getElementById('logout');
+    var err = "Thanks and come back again!"
+    this.setState({err:err});
+
+    lout.classList.add('hide');
+  }
 
   constructor(props){
     super(props);
@@ -64,6 +77,7 @@ class Authen extends Component {
     };
     this.login = this.login.bind(this);
     this.signup = this.signup.bind(this);
+    this.logout = this.logout.bind(this);
   }
   render(){
     return(
@@ -73,7 +87,7 @@ class Authen extends Component {
         <p>{this.state.err}</p>
         <button onClick = {this.login}>Log in</button>
         <button onClick = {this.signup}>Sign up</button>
-        <button id = 'logout' className = 'hide'>Log out</button>
+        <button onClick = {this.logout} id = 'logout' className = 'hide'>Log out</button>
       </div>
     );
   }
